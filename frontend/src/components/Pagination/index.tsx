c324e634af0ca99eabb7e4fc24a6a262038d96cc
augusto <gutim160@gmail.com>
2021-09-10 14:37:12 -0300

    ajuste de CSS

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -12,14 +12,14 @@ const Pagination = ({ page, onPageChange } : Props) => {
         <div className="row d-flex justify-content-center">
             <nav>
                 <ul className="pagination">
-                    <li className={`page-item ${page.first ? 'disabled' : ''}`}>
-                        <button className="page-link" onClick={() => onPageChange(page.number-1)}>Anterior</button>
+                    <li className={`page-item ${page.first ? 'disabled' : ''} `}>
+                        <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
                     </li>
                     <li className="page-item disabled">
                         <span className="page-link">{page.number + 1}</span>
                     </li>
-                    <li className={`page-item ${page.last ? 'disabled' : ''}`}>
-                        <button className="page-link" onClick={() => onPageChange(page.number+1)}>Próxima</button>
+                    <li className={`page-item ${page.last ? 'disabled' : ''} `}>
+                        <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima</button>
                     </li>
                 </ul>
             </nav>
